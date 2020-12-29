9f1963c2e21c82563fc9e6235fdfa96b94a557a3
lepetit <1299012718@qq.com>
2020-12-29 11:34:28 +0800

    Use scrapy to replace the request

--- Crossminds/Crossminds/spiders/crossminds.py
+++ Crossminds/Crossminds/spiders/crossminds.py
@@ -1,79 +1,76 @@
 # -*- coding: utf-8 -*-
-import scrapy
-import time
 import json
-from bs4 import BeautifulSoup
-from scrapy.utils.project import get_project_settings
+import random
+import time
+
+import scrapy
 from scrapy.crawler import CrawlerProcess
-from scrapy.http import Request,FormRequest
-from Crossminds.settings import DEFAULT_REQUEST_HEADERS
+from scrapy.http import Request
+from scrapy.utils.project import get_project_settings
+from tqdm import tqdm
+
 from Crossminds.items import CrossmindsItem
+from Crossminds.settings import DEFAULT_REQUEST_HEADERS
+
+
+def get_conferences(data):
+    # 从返回的 json 字符串中获得 json 数据
+    my_json = json.loads(data)
+
+    conferences = my_json['results'][0]
+    conferences_categories = conferences['subcategory']
+    conference_names = []
+    for category in conferences_categories:
+        conference_names.append(category['name'])
+
+    return conference_names
+
+
+def parse_paper(data):
+    for info in data:
+        _id = info['_id']
+        author = {
+            'name': info['author']['name'],
+            'email': info['author']['email']
+        }
+        title = info['title']
+        description = info['description']
+        video_source = info['source']
+        video_url = info['video_url']
+
+        yield _id, author, title, description, video_source, video_url
 
 
 class CrossmindsSpider(scrapy.Spider):
-       name = 'Crossminds'
-       start_url = 'https://api.crossminds.io/content/category/parents/details'
-       second_url = 'https://api.crossminds.io/web/content/bycategory'
+    name = 'Crossminds'
+    start_url = 'https://api.crossminds.io/content/category/parents/details'
+    second_url = 'https://api.crossminds.io/web/content/bycategory'
 
-       def start_requests(self):
+    def start_requests(self):
+        yield Request(url=self.start_url, headers=DEFAULT_REQUEST_HEADERS, callback=self.parse)
 
-              yield Request(url=self.start_url, headers=DEFAULT_REQUEST_HEADERS, callback=self.parse)
+    def parse(self, response, **kwargs):
+        # 获取会议列表
+        conferences = get_conferences(response.text)
+        # 获取每一个会议
+        for conference in tqdm(conferences):
+            time.sleep(random.uniform(1, 5))
+            body = json.dumps({'limit': 5000, 'offset': 0, 'search': {'category': conference}})
+            yield Request(url=self.second_url, method='post', headers=DEFAULT_REQUEST_HEADERS, body=body, callback=self.parse_detail)
 
-       def parse(self, response):
-              try:
-                     if (response.status != 200 and response.status != 304):
-                            return
-                     if (response.encoding is None):
-                            return
-                     elif (response.encoding != 'utf-8' and response.encoding != 'unicodeescape'):
-                            return
-                     else:
-                            if (response.text != "" or response.text is not None):
-                                   my_json = json.loads(response.text)
-                                   conference = my_json['results'][0]['subcategory']
-                                   limit = 24
-                                   for category,offset in zip(conference,range(5)):
-                                          data ={
-                                                 'limit': limit,
-                                                 'offset': offset,
-                                                 'search': {
-                                                 'category': category['name']
-                                                 },
-                                          }
-                                          final_data = json.dumps(data)
-                                          yield Request(url=self.second_url,headers=DEFAULT_REQUEST_HEADERS,method='POST',body=final_data,callback=self.parse_paper)
-                                          # yield FormRequest(url=self.second_url, headers=DEFAULT_REQUEST_HEADERS, callback=self.parse_paper,formdata=final_data)
-              except Exception as e:
-                     print(e)
+    def parse_detail(self, response):
+        info = CrossmindsItem()
+        papers = json.loads(response.text)
+        for _id, author, title, description, video_source, video_url in parse_paper(papers['results']):
+            info['_id'] = _id
+            info['title'] = title
+            info['authors'] = author['name']
+            info['description'] = description
+            info['videoUrl'] = video_url
+            yield info
 
-
-       def parse_paper(self, response):
-              info = CrossmindsItem()
-              try:
-                     if (response.status != 200 and response.status != 304):
-                            return
-                     if (response.encoding is None):
-                            return
-                     elif (response.encoding != 'utf-8' and response.encoding != 'unicodeescape'and response.encoding != 'cp1252'):
-                            return
-                     else:
-                           if (response.text != "" or response.text is not None): 
-                                   my_json = json.loads(response.text)
-                                   results = my_json['results']
-                                   for result in results:
-                                          info['title'] = result['title']
-                                          info['video_url'] = result['video_url']
-                                          info['author_email'] = result['author']['email']
-                                          # info['time'] = result['updated_at']
-                                          info['author'] = result['author']['name']
-                                          info['author_photo'] = result['author']['photoURL']
-                                          info['description'] = result['description']
-                                          info['author_id'] = result['author_id']
-                                          yield info
-              except Exception as e:
-                     print(e)
 
 if __name__ == "__main__":
-    process  = CrawlerProcess(get_project_settings())
+    process = CrawlerProcess(get_project_settings())
     process.crawl(CrossmindsSpider)
     process.start()
