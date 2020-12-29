612af9e8ca710f8411c0865cae8298e3a1166616
lepetit <1299012718@qq.com>
2020-12-29 23:48:39 +0800

    Add pdf item

--- Crossminds/Crossminds/items.py
+++ Crossminds/Crossminds/items.py
@@ -41,3 +41,9 @@ class CrossmindsItem(scrapy.Item):
     description = scrapy.Field()
     source = scrapy.Field()
     pass
+
+
+class PDFItem(scrapy.Item):
+    file_urls = scrapy.Field()  # 指定文件下载的连接
+    files = scrapy.Field()  # 文件下载完成后会往里面写相关的信息
+    file_names = scrapy.Field()
