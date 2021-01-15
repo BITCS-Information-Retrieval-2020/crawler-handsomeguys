0a724373a9c74180f0c0fd9df632e65d95ee08a9
Zhou Shelf <chefinclub@gmail.com>
2021-01-15 15:40:39 +0800

    return duplicate checksum info

--- handsomemongo.py
+++ handsomemongo.py
@@ -3,10 +3,10 @@ import string
 
 import pymongo
 from pymongo.errors import DuplicateKeyError
-
+from pymongo.database import Database
 
 class HandsomeMongo(object):
-    def __init__(self, db, target_collection) -> None:
+    def __init__(self, db:Database, target_collection:str) -> None:
         """
         理应只创建一个对象，涉及到多线程并发时候调用同一个对象的方法。类似pipelines中的elf.PaperSpiderItem
         封装地比较简单，有接口需要加参数的我再加
@@ -21,6 +21,11 @@ class HandsomeMongo(object):
         # self.delete_many=self.target_coll.delete_many
         self.find = self.target_coll.find
         self.find_one = self.target_coll.find_one
+        # 警告⚠：严禁更新标题和作者，应先删后插，其他内容请自便。
+        self.update = self.target_coll.update
+        self.update_one = self.target_coll.update_one
+        self.update_many = self.target_coll.update_many
+        self.find_one_and_update = self.target_coll.find_one_and_update
 
     def construct_checksumdoc(self, doc) -> dict:
         doc_title = doc["title"]
@@ -43,10 +48,10 @@ class HandsomeMongo(object):
             doc_authors_capital.append(author_cap.lower())
         doc_authors_capital.insert(0, doc_title)
         doc_checksum = zlib.adler32((" ".join(doc_authors_capital)).encode(encoding="utf-8"))
-        doc4checksum = {"_id": doc_checksum, "title": doc_title, "authors": doc["authors"]}
+        doc4checksum = {"_id": doc_checksum, "title": doc["title"], "authors": doc["authors"], "origin_id": doc["_id"], "originid": doc["id"]}
         return doc4checksum
 
-    def insert_one(self, doc) -> int:
+    def insert_one(self, doc) -> (int, dict) :
         '''
         确保作者以“, ”分隔（英文的逗号和空格）
         返回0（self.SUCESSFUL)表示成功，返回1（self.DUPLICATEKEY）表示是重复的
@@ -58,10 +63,10 @@ class HandsomeMongo(object):
             # 重了
             dup_doc = self.checksum_coll.find_one({"_id": doc4checksum["_id"]})
             print("The paper {} is duplicate with another paper {}".format(doc, dup_doc))
-            return self.DUPLICATEKEY
+            return (self.DUPLICATEKEY, dup_doc)
         else:
             self.target_coll.insert_one(doc)
-            return self.SUCESSFUL
+            return (self.SUCESSFUL, None)
 
     def delete_one(self, filter) -> int:
         '''
