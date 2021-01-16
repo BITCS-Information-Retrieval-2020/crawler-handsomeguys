0404cf8721dbbe7c479ab211ed741519cc9371d8
lepetit <1299012718@qq.com>
2021-01-16 19:25:47 +0800

    Change strategy of format title

--- Crossminds/format.py
+++ Crossminds/format.py
@@ -3,10 +3,10 @@ import re
 
 
 def format_title(title):
-    # title = re.sub(r'Session\s*\w+\s*-\s*', '', title)
-    # title = re.sub(r'SIGIR\s*\w*\s*-\s*', '', title)
-    # title = re.sub(r'\[[\x00-\x7F]+?]\s*', '', title)  # 去掉中括号
-    # title = re.sub(r'(\([\x00-\x7F]*?\))', '', title)  # 去掉小括号
+    title = re.sub(r'Session\s*\w+\s*-\s*', '', title)
+    title = re.sub(r'SIGIR\s*\w*\s*-\s*', '', title)
+    title = re.sub(r'\[[\x00-\x7F]+?]\s*', '', title)  # 去掉中括号
+    title = re.sub(r'(\([\x00-\x7F]*?\))', '', title)  # 去掉小括号
     title = re.sub(r'[A-Z][A-Z]+(\-[A-Z]+)?(\s|\')?([0-9]{2,4}|paper)(\soral\spaper)?', '~', title)
     title = re.sub(r'(Teaser|(short\s)?presentation|10m\stalk|Oral|Spotlight|Presentation|Talk|OralPresentation|Pitch\sVideo)', '~', title)
     title = re.sub(r'([\|\-\—\:,]\s?~|~\s?[\|\—\-\:,]|~\s?[\|\—\-\:,]\s?~)', '~', title)
