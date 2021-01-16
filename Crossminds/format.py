2f50af98dd9ecc2cb39ea132dbd9b35cd732b364
lepetit <1299012718@qq.com>
2021-01-16 19:32:49 +0800

    Fix a bug on format title

--- Crossminds/format.py
+++ Crossminds/format.py
@@ -8,7 +8,7 @@ def format_title(title):
     title = re.sub(r'\[[\x00-\x7F]+?]\s*', '', title)  # 去掉中括号
     title = re.sub(r'(\([\x00-\x7F]*?\))', '', title)  # 去掉小括号
     title = re.sub(r'[A-Z][A-Z]+(\-[A-Z]+)?(\s|\')?([0-9]{2,4}|paper)(\soral\spaper)?', '~', title)
-    title = re.sub(r'(Teaser|(short\s)?presentation|10m\stalk|Oral|Spotlight|Presentation|Talk|OralPresentation|Pitch\sVideo)', '~', title)
+    title = re.sub(r'(Teaser|(short\s)?\spresentation|10m\stalk|Oral|Spotlight|Presentation|Talk|OralPresentation|Pitch\sVideo)', '~', title)
     title = re.sub(r'([\|\-\—\:,]\s?~|~\s?[\|\—\-\:,]|~\s?[\|\—\-\:,]\s?~)', '~', title)
     title = re.sub(r'~.*?~', '', title)
     title = re.sub(r'~', '', title)
